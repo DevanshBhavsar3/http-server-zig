28e5efba58240299d9fbdb10de8e6e9445f62e00
Devansh Bhavsar <devanshbhavsar2006@gmail.com>
2025-04-08 12:20:53 +0000

    refactored the code

--- src/main.zig
+++ src/main.zig
@@ -5,49 +5,78 @@ const stdout = std.io.getStdOut().writer();
 
 const Response = struct { status: []const u8, headers: []const u8, body: []const u8 };
 
+const Request = struct {
+    requestLine: []const u8 = "",
+    header: []const u8 = "",
+    body: []const u8 = "",
+
+    pub fn parse(self: *Request, request: []u8) !void {
+        var data = std.mem.splitSequence(u8, request, "\r\n\r\n");
+
+        const requestLineAndHeader = data.first();
+        self.body = data.rest();
+
+        var fields = std.mem.splitSequence(u8, requestLineAndHeader, "\r\n");
+        self.requestLine = fields.first();
+        self.header = fields.rest();
+    }
+
+    pub fn getRoute(self: *Request) []const u8 {
+        var fields = std.mem.splitSequence(u8, self.requestLine, " ");
+        _ = fields.next();
+
+        return fields.next().?;
+    }
+
+    pub fn getHeader(self: *Request, header: []const u8) ?[]const u8 {
+        var fields = std.mem.splitSequence(u8, self.header, "\r\n");
+
+        while (fields.peek()) |field| : (_ = fields.next()) {
+            var key = std.mem.splitSequence(u8, field, ": ");
+
+            if (std.mem.eql(u8, key.first(), header)) {
+                return key.rest();
+            }
+        }
+
+        return null;
+    }
+};
+
 pub fn handleRequest(connection: std.net.Server.Connection) !void {
     defer connection.stream.close();
 
     try stdout.print("client connected!\n", .{});
 
-    var request: [128]u8 = undefined;
-    _ = try connection.stream.read(&request);
+    var requestData: [128]u8 = undefined;
+    _ = try connection.stream.read(&requestData);
 
-    var parts = std.mem.splitAny(u8, &request, " \r\n");
-    _ = parts.next().?;
+    var request = Request{};
+    try request.parse(&requestData);
+
     var reponse = Response{ .status = "", .headers = "", .body = "" };
 
-    if (std.mem.eql(u8, parts.peek().?, "/")) {
-        reponse.status = "HTTP/1.1 200 OK";
-    } else if (std.mem.startsWith(u8, parts.peek().?, "/echo")) {
-        var string = std.mem.splitAny(u8, parts.next().?, "/");
-        _ = string.next().?;
-        _ = string.next().?;
+    const route = request.getRoute();
 
-        const word = string.peek().?;
+    if (std.mem.eql(u8, route, "/")) {
+        reponse.status = "HTTP/1.1 200 OK";
+    } else if (std.mem.startsWith(u8, route, "/echo")) {
+        const word = route[6..];
 
         const headers = try std.fmt.allocPrint(std.heap.page_allocator, "Content-Type: text/plain\r\nContent-Length: {d}\r\n", .{word.len});
 
         reponse.status = "HTTP/1.1 200 OK";
         reponse.headers = headers;
         reponse.body = word;
-    } else if (std.mem.eql(u8, parts.peek().?, "/user-agent")) {
+    } else if (std.mem.eql(u8, route, "/user-agent")) {
+        const userAgent = request.getHeader("User-Agent").?;
+
         reponse.status = "HTTP/1.1 200 OK";
-
-        while (parts.peek() != null) {
-            const field = parts.next().?;
-
-            if (std.mem.eql(u8, field, "User-Agent:")) {
-                const userAgent = parts.peek().?;
-                reponse.body = userAgent;
-                reponse.headers = try std.fmt.allocPrint(std.heap.page_allocator, "Content-Type: text/plain\r\nContent-Length: {d}\r\n", .{userAgent.len});
-            }
-        }
+        reponse.headers = try std.fmt.allocPrint(std.heap.page_allocator, "Content-Type: text/plain\r\nContent-Length: {d}\r\n", .{userAgent.len});
+        reponse.body = userAgent;
     } else {
         reponse.status = "HTTP/1.1 404 Not Found";
     }
-
-    std.debug.print("Here", .{});
 
     try sendResponse(connection, reponse);
 }
